410c4e0703541c740d40faf7f034074c15a5752c
Dustin Richmond <6378612+drichmond@users.noreply.github.com>
2023-01-20 15:15:16 -0800

    Add mul2x2

---
+++ lab3/part1/mul2x2/mul2x2.sv
@@ -0,0 +1,10 @@
+module mul2x2
+  (input [1:0] a_i
+  ,input [1:0] b_i
+  ,output [3:0] c_o);
+
+   // Implement a 2-bit by 2-bit multiplier (2x2) using the LUT6_2 module.
+   // 
+   // Your code here:
+
+endmodule
