eb08da2c1024a8f9410366a74d784b184d194bec
vboxuser <vboxuser@CSE-x25-VBox-VM.myguest.virtualbox.org>
2023-02-06 15:56:34 -0800

    Working on part2

--- lab3/part2/ram_1w1r_async/ram_1w1r_async.sv
+++ lab3/part2/ram_1w1r_async/ram_1w1r_async.sv
@@ -16,10 +16,26 @@ module ram_1w1r_async
    // Implement a parameterized, write-priority/write-first asynchronous RAM.
    // 
    // Your code here:
+   logic [width_p - 1:0] mem [depth_p - 1:0];
+   
+   assign rd_data_o = (rd_addr_i == wr_addr_i) ? wr_data_i : mem[rd_addr_i];
+   
+   always_ff @(posedge clk_i) begin
+       if (wr_valid_i) begin
+           mem[wr_addr_i] <= wr_data_i;
+       end
+   end
+   
+   always_ff @(posedge clk_i) begin
+       if(reset_i) begin
+           $readmemh("memory_init_file.hex", mem);
+       end
+   end
 
-   initial begin
+   /*initial begin
       // Display depth and width (You will need to match these in your init file
       $display("%m: depth_p is %d, width_p is %d", depth_p, width_p);
-   end
+      $readmemh("memory_init_file.hex", mem);
+   end*/
        
 endmodule
